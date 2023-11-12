4b39aa45390e4c7973663d90b1e8e6dafeb515fa
ihsannafishidayat <ihsannafishidayat@gmail.com>
2023-11-13 00:00:53 +0700

    test

--- src/cobacoba.java
+++ src/cobacoba.java
@@ -1,6 +1,6 @@
 public class cobacoba {
     public static void main(String[] args) {
         System.out.println("coba test koneksi ke github");
-        System.out.println("angka");
+        System.out.println("nama orang");
     }
 }
